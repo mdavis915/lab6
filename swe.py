486c0a9d554c2e7ad6c4a5793b7533d96db2ada9
Maria Davis <davismaria@ufl.edu>
2023-10-25 18:30:57 -0400

    test1

---
+++ swe.py
@@ -0,0 +1,20 @@
+def decoder(password):
+    new_p = ""
+    
+    for digit in password:
+        new_d = str((int(digit) - 3) % 10)
+        new_p += new_d
+    return new_p
+
+
+def main():
+    
+    while True:
+
+        
+        option = input("Please enter an option: ")
+        password = input("Please enter your password to encode: ")
+        print("Your password has been encoded and stored!")
+        
+        if option == 1:
+            pass
